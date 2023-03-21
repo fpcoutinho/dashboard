d5224441b8ea57175d009525ca254c625668406a
fpcoutinho <fpcoutinho5@gmail.com>
2023-03-21 17:30:53 -0300

    função seletora 'active' de link menu implementado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const navMenu = document.querySelectorAll('.nav-link');
+
 document.querySelector(".botao-tema").addEventListener("click", switchTheme);
 function switchTheme() {
     const root = document.documentElement;
@@ -5,7 +7,7 @@ function switchTheme() {
     root.className = newTheme;
 }
 
-window.onload = () => {
+function checkUserTheme(){
     const root = document.documentElement;
     const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
     if(prefersDarkMode) {
@@ -14,4 +16,29 @@ window.onload = () => {
     else {
         root.className = 'light';
     }
+}
+
+navMenu.forEach(link => {
+    link.addEventListener('click', set => {
+        setActive(link);
+        //loadSection(link.getAttribute('href'));
+    });
+});
+
+function setActive(navItem){
+    navMenu.forEach((link) => {
+        if(link.classList.contains('active'))
+            link.classList.remove('active');
+    });
+    navItem.classList.add('active');
+}
+
+function loadSection(href) {
+    const main = document.querySelector('.main');
+    main.innerHTML = '';
+    main.innerHTML = `<h1>${href}</h1>`;
+}
+
+window.onload = () => {
+    checkUserTheme();
 }
